03dbf7412ddf430599761e2e02553109344f232b
Maximch1 <maximch@bk.ru>
2017-03-24 22:30:26 +0300

    исправления замечаний

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@
  */
 
 var Page = document.querySelector('.page-header__wrapper');
-var Menu1 = document.querySelector('.menu');
+var Menu1 = document.querySelector('.menu__catalog');
 var Menu2 = document.querySelector('.menu__search');
 var Button = document.querySelector('.menu__button');
 Page.classList.add('page-header__wrapper--js');
-Menu1.classList.add('menu--js');
+Menu1.classList.add('menu__catalog--js');
 Menu2.classList.add('menu__search--js');
 
 Button.addEventListener('click', function() {
-  Menu1.classList.toggle('menu--js');
+  Menu1.classList.toggle('menu__catalog--js');
   Menu2.classList.toggle("menu__search--js");
   Button.classList.toggle('menu__button--close');
 });
