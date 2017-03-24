9a732ef1175add7c24ea1d4dabecdfdd1f03dea6
Maximch1 <maximch@bk.ru>
2017-03-24 17:57:58 +0300

    исправления

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task("style", function() {
 gulp.task("serve", ["style"], function() {
   server.init({
     server: ".",
-    index: "catalog.html",
+    index: "index.html",
     notify: false,
     open: true,
     cors: true,
